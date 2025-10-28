cd43d070117e010ccd7f1422fb7d125dca1b0a53
AlyssaU3 <alys4321c@gmail.com>
2025-10-28 15:37:44 -0400

    63 done

--- src/missing_value_types.py
+++ src/missing_value_types.py
@@ -4,9 +4,11 @@ import pandas as pd
 import numpy as np
 
 def missing_value_types():
-   
-    pass
-               
+    country_name= ["United Kingdom","Finland", "USA", "Sweden", "Germany", "Russia"]
+    independance= [np.nan, 1917, 1776, 1523, np.nan, 1992 ]
+    president=[None, "Niinistö", "Trump",None, "Steinmeier", "Putin"]
+    countys= pd.DataFrame({"Year of independence": independance, "President": president}, index= country_name)
+    return countys 
 def main():
     print(missing_value_types())
 
